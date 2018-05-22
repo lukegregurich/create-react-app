f2e9fe1255621279478ad9d816947a35d87ea08d
Luke Gregurich <lgreguri@purdue.edu>
2018-05-22 10:58:45 -0400

    add main component

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import Main from  './Main'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="Main">
+                <Main />
+            </div>
+        )
+    }
+}
+
+export default App
